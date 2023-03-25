8f86940d607a70ae99a233102f17fec2b4757912
Orjiugo Victor <106749319+theorjiugovictor@users.noreply.github.com>
2023-03-25 20:16:47 +0100

    Update get_precision.c

--- get_precision.c
+++ get_precision.c
@@ -8,51 +8,34 @@
 *
 * Return: Precision.
 */
-int get_precision(const char *format, ...)
+int get_precision(const char *format, int *i, va_list list)
 {
-	va_list args;
-	va_start(args, format);
-	
-	int written = 0;
-	while (*format != '\0') {
+	int curr_i = *i + 1;
+	int precision = -1;
 
-	if (*format == '%') {
-		++format;
-		int precision = -1;
-		if (*format == '.') {
-			++format;
-			precision = 0;
-			while (is_digit(*format)) {
-				precision *= 10;
-				precision += *format - '0';
-				++format;
-			}
+	if (format[curr_i] != '.')
+		return (precision);
+
+	precision = 0;
+
+	for (curr_i += 1; format[curr_i] != '\0'; curr_i++)
+	{
+		if (is_digit(format[curr_i]))
+		{
+			precision *= 10;
+			precision += format[curr_i] - '0';
 		}
-	if (*format == 'd') {
-		int value = va_arg(args, int);
-		written += printf("%.*d", precision, value);
-	} else if (*format == 'f') {
-		double value = va_arg(args, double);
-		written += printf("%.*f", precision, value);
-	} else if (*format == 's') {
-		char *value = va_arg(args, char*);
-		written += printf("%.*s", precision, value);
-	}
-	} else {
-		putchar(*format);
-		++written;
-	}
-		++format;
+		else if (format[curr_i] == '*')
+		{
+			curr_i++;
+			precision = va_arg(list, int);
+			break;
+		}
+		else
+			break;
 	}
 
-	va_end(args);
-	return written;
-}
+	*i = curr_i - 1;
 
-int main()
-{
-	my_printf("Printing an integer: %d\n", 123);
-	my_printf("Printing a float with precision: %.2f\n", 3.14159);
-	my_printf("Printing a string with precision: %.4s\n", "hello world");
-	return 0;
+	return (precision);
 }
