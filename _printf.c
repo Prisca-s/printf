5fc4a1bf51bcea4ef8c50c044760f1dabbcae8c4
Orjiugo Victor <106749319+theorjiugovictor@users.noreply.github.com>
2023-03-25 15:06:44 +0100

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -2,9 +2,9 @@
 
 void print_buffer(char buffer[], int *buff_ind);
 /**
-* _printf - printf function
-* @format: format.
-* Return: Printed chars.
+*_printf - printf function
+*@format: format.
+*Return: Printed chars.
 */
 int _printf(const char *format, ...)
 {
